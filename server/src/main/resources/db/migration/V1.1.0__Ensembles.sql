5e907c999957202f11dc89035af720e3de0948bd
Fabian Halbmann <fabianhalbmann@web.de>
2026-02-20 11:13:16 +0100

    adapt flyway migration script (added new fields: minCount, targetCount, maxCount)

--- server/src/main/resources/db/migration/V1.1.0__Ensembles.sql
+++ server/src/main/resources/db/migration/V1.1.0__Ensembles.sql
@@ -26,14 +26,17 @@ create table ensembles_AUD
 
 create table ensemble_voices
 (
-    id          uuid         not null,
+    id          uuid             not null,
     created     timestamp(6),
     lastUpdate  timestamp(6),
-    version     integer      not null,
-    label       varchar(255) not null,
+    version     integer          not null,
+    label       varchar(255)     not null,
     weight      double precision not null,
-    required    boolean      not null,
-    ensemble_id uuid         not null,
+    required    boolean          not null,
+    minCount    integer,
+    targetCount integer,
+    maxCount    integer,
+    ensemble_id uuid             not null,
     primary key (id)
 );
 
@@ -46,6 +49,9 @@ create table ensemble_voices_AUD
     label       varchar(255),
     weight      double precision,
     required    boolean,
+    minCount    integer,
+    targetCount integer,
+    maxCount    integer,
     ensemble_id uuid,
     primary key (REV, id)
 );
@@ -79,40 +85,40 @@ create table voice_options_AUD
 -- Foreign keys for ensembles audit
 alter table if exists ensembles_AUD
     add constraint FK_ensembles_AUD_REV
-        foreign key (REV) references REVINFO;
+    foreign key (REV) references REVINFO;
 
 alter table if exists ensembles_AUD
     add constraint FK_ensembles_AUD_REVEND
-        foreign key (REVEND) references REVINFO;
+    foreign key (REVEND) references REVINFO;
 
 -- Foreign keys for ensemble_voices
 alter table if exists ensemble_voices
     add constraint FK_ensemble_voices_ensemble
-        foreign key (ensemble_id) references ensembles;
+    foreign key (ensemble_id) references ensembles;
 
 -- Foreign keys for ensemble_voices audit
 alter table if exists ensemble_voices_AUD
     add constraint FK_ensemble_voices_AUD_REV
-        foreign key (REV) references REVINFO;
+    foreign key (REV) references REVINFO;
 
 alter table if exists ensemble_voices_AUD
     add constraint FK_ensemble_voices_AUD_REVEND
-        foreign key (REVEND) references REVINFO;
+    foreign key (REVEND) references REVINFO;
 
 -- Foreign keys for voice_options
 alter table if exists voice_options
     add constraint FK_voice_options_voice
-        foreign key (voice_id) references ensemble_voices;
+    foreign key (voice_id) references ensemble_voices;
 
 alter table if exists voice_options
     add constraint FK_voice_options_instrument
-        foreign key (instrument_id) references instruments (id);
+    foreign key (instrument_id) references instruments (id);
 
 -- Foreign keys for voice_options audit
 alter table if exists voice_options_AUD
     add constraint FK_voice_options_AUD_REV
-        foreign key (REV) references REVINFO;
+    foreign key (REV) references REVINFO;
 
 alter table if exists voice_options_AUD
     add constraint FK_voice_options_AUD_REVEND
-        foreign key (REVEND) references REVINFO;
+    foreign key (REVEND) references REVINFO;
